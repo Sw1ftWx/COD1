7d17895f18e901db8afcfb63feb0add7ecde261f
Wing Fung Wu <ryanwu7275@gmail.com>
2023-03-08 06:59:58 -0800

    save

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
+def add(num1, num2):
+    sum = num1 + num2
+    return sum
+
+def main():
+    a = 1
+    b = 2
+    sum = add(a, b)
+    print(sum)
+
 if __name__ == '__main__':
-    print(2)
+    main()
     
