30be6abbd231e9c9cde809acb485a94a1c21a643
Sw1ftWx <wangxunsw1ft@gmail.com>
2023-03-09 00:50:26 +0900

    24

---
+++ aa.py
@@ -0,0 +1,28 @@
+def add(num1, num2):
+    sum = num1 + num2
+    return sum 
+
+def main():
+    a = 1
+    b = 2
+    sum = add(a,b)
+    print(sum)
+    return sum
+
+if __name__ == '__main__':
+    main
+
+if __name__ == '__main__':
+    print('add 1 and 2 is {add(1,2)}')
+
+i = 2
+while i < 10:
+    print('caonima')
+    i += 1
+       
+input('please type something')
+
+
+
+    
+    
